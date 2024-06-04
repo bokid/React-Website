c84943629e7361dda75db1aeb8913bb207eaabbb
Ben Orr <benjaminbordwell@gmail.com>
2024-06-04 11:00:25 -0400

    change app

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/React-Website" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
